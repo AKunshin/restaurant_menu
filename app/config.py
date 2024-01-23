6d5e3475a8148b153a514efaceb089dc9e1e1749
AKunshin <reyiby2015@gmail.com>
2024-01-23 16:43:10 +0300

    Add config for test_db connections

--- app/config.py
+++ app/config.py
@@ -3,18 +3,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Режим работы приложения
     MODE: Literal["DEV", "TEST", "PROD"]
 
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 5432
-    DB_USER: str = "rest_user"
-    DB_NAME: str = "restaurant_db"
-    DB_PASS: str = "pass"
+    # Данные для подключения к рабочей БД
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_NAME: str
+    DB_PASS: str
 
     @property
     def database_url(self):
+        """Получение DSN рабочей БД"""
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
+
+    # Данные для подключения к тестовой БД
     TEST_DB_HOST: str = "localhost"
     TEST_DB_PORT: int = 5432
     TEST_DB_USER: str = "rest_user"
@@ -23,6 +27,7 @@ class Settings(BaseSettings):
 
     @property
     def test_database_url(self):
+        """Получение DSN тестовой БД"""
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
@@ -5,10 +6,12 @@ from app.config import settings
 
 if settings.MODE == "TEST":
     DATABASE_URL = settings.test_database_url
+    DATABASE_PARAMS = {"poolclass": NullPool}
 else:
     DATABASE_URL = settings.database_url
+    DATABASE_PARAMS = {}
 
-engine = create_async_engine(url=DATABASE_URL, echo=True)
+engine = create_async_engine(url=DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
