6151d5452ec702b529cba471cc388972523adc28
Анатолий Куншин <reyiby2015@gmail.com>
2024-01-29 00:11:05 +0300

    write SQL query for orm

--- app/menu/utils.py
+++ app/menu/utils.py
@@ -1,5 +1,5 @@
 from loguru import logger
-from sqlalchemy import func, select
+from sqlalchemy import func, select, join
 from app.database import async_session_maker
 from app.menu.models import Menu
 from app.submenu.models import Submenu
@@ -9,17 +9,20 @@ from app.dish.models import Dish
 async def get_menu_with_counts(id):
     async with async_session_maker() as session:
         """
-        select count(id) as submenus_count
-        from submenus
-        where menu_id='1150a2e8-1aa6-49c8-a562-f0903dcfd990';
-         SELECT count(submenus.id) AS submenus_count
-        FROM submenus
-        WHERE submenus.menu_id = $1::UUID
+        SELECT M.TITLE,
+        M.DESCRIPTION,
+        COUNT(S.ID) AS SUBMENUS_COUNT,
+        COUNT(D.ID) AS DISHES_COUNT
+        FROM MENUS AS M
+        JOIN SUBMENUS AS S ON S.MENU_ID = M.ID
+        JOIN DISHES AS D ON D.SUBMENU_ID = S.ID
+        WHERE M.ID = '1150a2e8-1aa6-49c8-a562-f0903dcfd990'
+        GROUP BY M.ID;
         """
-        stmt = select(func.count(Submenu.id).label("submenus_count")).filter(Submenu.menu_id == id)
-
+        j = join(Menu, Submenu, Menu.id == Submenu.menu_id, isouter=True)
+        stmt = select(Menu).select_from(j)
 
         result = await session.execute(stmt)
-        result = result.scalar_one()
-        logger.debug(f"{result=}")
-        return result
+        # result = result.scalars()
+        # logger.debug(f"{result=}")
+        return result.scalars().all()
